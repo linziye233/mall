7957c9f12d32f1dce65eab8fc4238562e9e50f5b
linziye <linziye719@163.com>
2026-05-09 14:06:22 +0800

    feat: 添加订单模块

---
+++ mall-order/src/main/java/com/test/mall/domain/PortalMemberInfo.java
@@ -0,0 +1,10 @@
+package com.test.mall.domain;
+
+import com.test.mall.model.UmsMember;
+import com.test.mall.model.UmsMemberLevel;
+import lombok.Data;
+
+@Data
+public class PortalMemberInfo extends UmsMember {
+    private UmsMemberLevel umsMemberLevel;
+}
---
+++ mall-order/src/main/java/com/test/mall/service/OmsPortalOrderReturnApplyService.java
@@ -0,0 +1,15 @@
+package com.test.mall.service;
+
+
+import com.test.mall.domain.OmsOrderReturnApplyParam;
+
+/**
+ * 订单退货管理Service
+ * Created by macro on 2018/10/17.
+ */
+public interface OmsPortalOrderReturnApplyService {
+    /**
+     * 提交申请
+     */
+    int create(OmsOrderReturnApplyParam returnApply);
+}
