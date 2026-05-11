1692f66b8394e46c69053033b11696c9edfe7492
linziye <linziye719@163.com>
2026-05-11 21:02:15 +0800

    feat: 添加canal服务

---
+++ mall-canal/src/main/java/com/test/MallCanalApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCanalApplication.class, args);
+    }
+
+}
