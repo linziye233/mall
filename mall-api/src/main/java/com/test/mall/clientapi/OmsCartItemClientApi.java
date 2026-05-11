bc12fcc6fffe51b89759e2a026112499e49fd5bd
linziye <linziye719@163.com>
2026-05-11 20:58:38 +0800

    feat: 统一feign调用模块

---
+++ mall-api/src/main/java/com/test/mall/clientapi/OmsCartItemClientApi.java
@@ -0,0 +1,17 @@
+package com.test.mall.clientapi;
+
+/**
+* @vlog: 高于生活，源于生活
+* @desc: 类的描述:远程调用订单中心购物车详细客户端
+* @author: smlz
+* @createDate: 2020/1/14 22:17
+* @version: 1.0
+*/
+/*@FeignClient(name = "tulingmall-order")
+public interface OmsCartItemClientApi {
+
+    @RequestMapping(value = "/list/promotion", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<CartPromotionItem>> listPromotion();
+
+}*/
---
+++ mall-api/src/main/java/com/test/mall/clientapi/UmsMemberClientApi.java
@@ -0,0 +1,24 @@
+package com.test.mall.clientapi;
+
+import com.test.mall.common.api.CommonResult;
+import com.test.mall.config.UmsMemberFeignConfig;
+import com.test.mall.model.UmsMember;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+* @vlog: 高于生活，源于生活
+* @desc: 类的描述:Feign远程调用用户服务接口
+* @author: smlz
+* @createDate: 2020/1/14 21:44
+* @version: 1.0
+*/
+@FeignClient(name = "tulingmall-member",configuration = UmsMemberFeignConfig.class)
+public interface UmsMemberClientApi {
+
+    @RequestMapping(value = "/getCurrentMember", method = RequestMethod.GET)
+    @ResponseBody
+    CommonResult<UmsMember> getCurrentMember();
+}
